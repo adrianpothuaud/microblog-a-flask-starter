f069f2e15d1e7102d3c806bf6e910d61dcd9243b
Adrian Pothuaud <111364860+apothuoctober@users.noreply.github.com>
2022-10-18 10:06:41 +0200

    add db and migrate configuration + User model

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
@@ -8,5 +10,11 @@ app = Flask(__name__)
 # Application configuration
 app.config.from_object(Config)
 
+# Database instance
+db = SQLAlchemy(app)
+
+# Migrate instance
+migrate = Migrate(app, db)
+
 # Import at the bottom to resolve circular dependencies
-from app import routes
+from app import routes, models
