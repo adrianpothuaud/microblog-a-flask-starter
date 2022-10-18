7254b8fc9111debf9aceff8914bec09ad64b6a80
Adrian Pothuaud <111364860+apothuoctober@users.noreply.github.com>
2022-10-18 09:19:24 +0200

    add application configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 
+from config import Config
+
 # Creates the application object as instance of class Flask
 app = Flask(__name__)
+
+# Application configuration
+app.config.from_object(Config)
 
 # Import at the bottom to resolve circular dependencies
 from app import routes
