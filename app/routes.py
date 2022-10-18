1d72c449d1a5ae9c900b52769ea043ecb9509779
Adrian Pothuaud <111364860+apothuoctober@users.noreply.github.com>
2022-10-18 09:29:08 +0200

    add login view

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 
 @app.route("/")
@@ -15,3 +16,9 @@ def index():
     ]
     # Renders a template
     return render_template("pages/index.html", posts=posts, title="Home", user=adrian)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("pages/login.html", title="Login", form=form)
